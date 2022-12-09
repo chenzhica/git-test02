85c5987b446e229b0cbf3c53a6499faaec27658b
Chenzhicai <995235607@qq.com>
2022-12-09 13:01:58 +0800

    first commit

---
+++ src/main/java/com/itheima/GitTset.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class GitTset {
+    public static void main(String[] args) {
+        System.out.println("hello~git");
+        System.out.println("hello~git");
+        System.out.println("hello~git");
+
+    }
+}
