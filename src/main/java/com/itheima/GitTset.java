a41ce39febec21c90b27a180a9c0b549bf816d7f
Chenzhicai <995235607@qq.com>
2022-12-09 13:14:51 +0800

    634612c3 firt commit

--- src/main/java/com/itheima/GitTset.java
+++ src/main/java/com/itheima/GitTset.java
@@ -6,6 +6,7 @@ public class GitTset {
         System.out.println("hello~git");
         System.out.println("hello~git");
         System.out.println("hello~git");
+        System.out.println("master  secnd  commit");
 
 
     }
